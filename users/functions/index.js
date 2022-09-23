12aeb05bc82e59e8fc77ec0ef0a172d9e3f63dea
stanleyphu <phu.stanley@gmail.com>
2022-09-22 22:22:07 -0700

    Update login method to create warrant session token

--- users/functions/index.js
+++ users/functions/index.js
@@ -27,13 +27,26 @@ exports.setUserClaims = functions.auth.user().beforeSignIn(async (user, _) => {
     apiKey: "YOUR_API_KEY",
   });
 
+  if (user.customClaims) {
+    if (!user.customClaims["http://warrant.dev"]) {
+      user.customClaims["http://warrant.dev"] = {};
+    }
+  } else {
+    user.customClaims = {"http://warrant.dev": {}};
+  }
+
   let roles = await warrantClient.listRolesForUser(user.uid);
   roles = roles.map((role) => role.roleId);
-  user.customClaims["roles"] = roles;
+  user.customClaims["http://warrant.dev"]["roles"] = roles;
 
   let permissions = await warrantClient.listPermissionsForUser(user.uid);
   permissions = permissions.map((permission) => permission.permissionId);
-  user.customClaims["permissions"] = permissions;
+  user.customClaims["http://warrant.dev"]["permissions"] = permissions;
+
+  const sessionToken = await warrantClient.createAuthorizationSession({
+    userId: user.uid,
+  });
+  user.customClaims["http://warrant.dev"]["sessionToken"] = sessionToken;
 
   return {
     customClaims: user.customClaims,
