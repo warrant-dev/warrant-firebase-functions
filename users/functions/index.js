8d2ddf338004e55d57cc26453170346525f8fc6d
stanleyphu <phu.stanley@gmail.com>
2023-03-01 14:59:14 -0800

    Update functions to use warrant-node v3

--- users/functions/index.js
+++ users/functions/index.js
@@ -4,41 +4,41 @@ const Warrant = require("@warrantdev/warrant-node");
 const WARRANT_NAMESPACE = "http://warrant.dev";
 
 exports.createWarrantUser = functions.auth.user().onCreate((user) => {
-  const warrantClient = new Warrant.Client({
+  const warrantClient = new Warrant.WarrantClient({
     apiKey: "YOUR_API_KEY",
   });
 
-  warrantClient
-      .createUser({userId: user.uid, email: user.email})
+  warrantClient.User
+      .create({userId: user.uid, email: user.email})
       .then((newUser) => console.log(newUser))
       .catch((error) => console.log(error));
 });
 
 exports.deleteWarrantUser = functions.auth.user().onDelete((user) => {
-  const warrantClient = new Warrant.Client({
+  const warrantClient = new Warrant.WarrantClient({
     apiKey: "YOUR_API_KEY",
   });
 
-  warrantClient
-      .deleteUser(user.uid)
+  warrantClient.User
+      .delete(user.uid)
       .catch((error) => console.log(error));
 });
 
 // Use this in conjunction with `setUserClaims` when you want to set claims on sign in
 // If you don't want to set claims on sign in, you may use the function above with the `onCreate` trigger
 exports.createWarrantUser = functions.auth.user().beforeCreate((user, _) => {
-  const warrantClient = new Warrant.Client({
+  const warrantClient = new Warrant.WarrantClient({
     apiKey: "YOUR_API_KEY",
   });
 
-  warrantClient
-      .createUser({userId: user.uid, email: user.email})
+  warrantClient.User
+      .create({userId: user.uid, email: user.email})
       .then((newUser) => console.log(newUser))
       .catch((error) => console.log(error));
 });
 
 exports.setUserClaims = functions.auth.user().beforeSignIn(async (user, _) => {
-  const warrantClient = new Warrant.Client({
+  const warrantClient = new Warrant.WarrantClient({
     apiKey: "YOUR_API_KEY",
   });
 
@@ -50,15 +50,15 @@ exports.setUserClaims = functions.auth.user().beforeSignIn(async (user, _) => {
     user.customClaims = {[WARRANT_NAMESPACE]: {}};
   }
 
-  let roles = await warrantClient.listRolesForUser(user.uid);
+  let roles = await warrantClient.Role.listRolesForUser(user.uid);
   roles = roles.map((role) => role.roleId);
   user.customClaims[WARRANT_NAMESPACE]["roles"] = roles;
 
-  let permissions = await warrantClient.listPermissionsForUser(user.uid);
+  let permissions = await warrantClient.Permission.listPermissionsForUser(user.uid);
   permissions = permissions.map((permission) => permission.permissionId);
   user.customClaims[WARRANT_NAMESPACE]["permissions"] = permissions;
 
-  const sessionToken = await warrantClient.createAuthorizationSession({
+  const sessionToken = await warrantClient.Session.createAuthorizationSession({
     userId: user.uid,
   });
   user.customClaims[WARRANT_NAMESPACE]["sessionToken"] = sessionToken;
